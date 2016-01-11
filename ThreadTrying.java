b4559676dfc2578bc21e0b475bc7fb43e8200ad4
birnur uzun <birnuruzun.294976@gmail.com>
2016-01-12 00:20:19 +0200

    "edit"

--- ThreadTrying.java
+++ ThreadTrying.java
@@ -4,6 +4,8 @@ public class ThreadTrying {
 	public static void main(String[] args){
 		Thread thread1 = new Thread(new MyThread("thread1",6));
 		Thread thread2 = new Thread(new MyThread("thread2",5),"thread2");
+		/*if we want to determine task priorities of the threads,
+		we can use "setPriority" method in this section.*/
 		thread1.setPriority(Thread.MIN_PRIORITY);
 		thread2.setPriority(Thread.MAX_PRIORITY);
 		thread1.start();
